ec23c33a0142b6957dfe36a4d0d1b95a671d2fa6
spicalous <benlertlumprasertkul@gmail.com>
2024-04-13 02:11:29 +0100

    image to img

--- src/.vuepress/theme/layouts/Home.vue
+++ src/.vuepress/theme/layouts/Home.vue
@@ -12,27 +12,27 @@ const site = useSiteData();
     <div class="row">
       <div class="col-12 col-md-6 position-relative pt-4">
         <span class="position-absolute top-50 start-50 translate-middle h1 text-white">Project 1</span>
-        <image class="img-fluid" :src="$withBase('/home-1.png')"/>
+        <img class="img-fluid" :src="$withBase('/home-1.png')"/>
       </div>
       <div class="col-12 col-md-6 position-relative pt-4">
         <span class="position-absolute top-50 start-50 translate-middle h1 text-white">Project 2</span>
-        <image class="img-fluid" :src="$withBase('/home-2.png')"/>
+        <img class="img-fluid" :src="$withBase('/home-2.png')"/>
       </div>
     </div>
     <div class="row pt-4">
       <div class="col position-relative">
         <span class="position-absolute top-50 start-50 translate-middle h1 text-white">Project 3</span>
-        <image class="img-fluid w-100" src="https://placehold.co/1000x500"/>
+        <img class="img-fluid w-100" src="https://placehold.co/1000x500"/>
       </div>
     </div>
     <div class="row pb-5">
       <div class="col-12 col-md-6 position-relative pt-4">
         <span class="position-absolute top-50 start-50 translate-middle h1 text-white">Project 4</span>
-        <image class="img-fluid" src="https://placehold.co/1000x700"/>
+        <img class="img-fluid" src="https://placehold.co/1000x700"/>
       </div>
       <div class="col-12 col-md-6 position-relative pt-4">
         <span class="position-absolute top-50 start-50 translate-middle h1 text-white">Project 5</span>
-        <image class="img-fluid" src="https://placehold.co/1000x700"/>
+        <img class="img-fluid" src="https://placehold.co/1000x700"/>
       </div>
     </div>
   </div>
