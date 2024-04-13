f4dcfc164dcacc6e3db2f2876d1534829ed4c784
spicalous <benlertlumprasertkul@gmail.com>
2024-04-13 01:39:29 +0100

    use templates

--- src/.vuepress/theme/theme.js
+++ src/.vuepress/theme/theme.js
@@ -7,6 +7,8 @@ export function localTheme(options) {
   return {
     name: 'local-theme',
     clientConfigFile: path.resolve(__dirname, 'client.js'),
+    templateBuild: path.resolve(__dirname, 'templates/build.html'),
+    templateDev: path.resolve(__dirname, 'templates/dev.html'),
     plugins: [
       themeDataPlugin({
         themeData: options
